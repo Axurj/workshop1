147efa549d898056a89862719900b0cd710b4432
Axurj <acerj2468@gmail.com>
2020-07-07 14:23:01 -0700

    Added 5 variables and 5 constants

---
+++ naming.js
@@ -0,0 +1,16 @@
+var name = "Jaye"
+var lastName = "Cheng"
+var cashStarted = 12; 
+var currentBalance = cashStarted - breadCost;
+var finalBalance = currentBalance;
+
+const breadCost = 4;
+const gumCost = 1;
+const yoyoCost = 3;
+const appleCost = 1;
+const gameCost = 60;
+
+
+console.log(name + " is buying bread for $" + breadCost + ", " + name + " has $" + cashStarted + ".");
+console.log("After buying the bread, she has $" + currentBalance + " left.");
+console.log(breadCost);
