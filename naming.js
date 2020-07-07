f9aa8d616029daeaeebee5155fe50d991483ca75
Axurj <acerj2468@gmail.com>
2020-07-07 14:34:33 -0700

    Added comments and prints

--- naming.js
+++ naming.js
@@ -1,16 +1,29 @@
-var name = "Jaye"
-var lastName = "Cheng"
-var cashStarted = 12; 
-var currentBalance = cashStarted - breadCost;
-var finalBalance = currentBalance;
-
+//Constants
 const breadCost = 4;
-const gumCost = 1;
+const gumCost = 2;
 const yoyoCost = 3;
 const appleCost = 1;
 const gameCost = 60;
 
+//Variables 
+var name = "Jaye"
+var lastName = "Cheng"
+var cashStarted = 12; 
+var currentBalance = cashStarted - breadCost;
+var fullName = name + " " + lastName;
 
+//Console.log (Prints out a story scenario)
 console.log(name + " is buying bread for $" + breadCost + ", " + name + " has $" + cashStarted + ".");
 console.log("After buying the bread, she has $" + currentBalance + " left.");
-console.log(breadCost);
+
+//Prints out all the variables and constants used.
+console.log(name)
+console.log(lastName)
+console.log(fullName)
+console.log(cashStarted)
+console.log(currentBalance)
+console.log(breadCost)
+console.log(gumCost)
+console.log(yoyoCost)
+console.log(appleCost)
+console.log(gameCost)
